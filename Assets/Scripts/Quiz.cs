31c6a1059961056aa370b23ebf33373b8150b001
tessla-muir <tesslamuir@yahoo.com>
2022-10-27 22:41:55 -0400

    Methods for button states and sprites

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -15,15 +15,18 @@ public class Quiz : MonoBehaviour
 
     void Start()
     {
+        GetNextQuestion();
         DisplayQuestion();
     }
 
     // Displays question and answers for a question
     void DisplayQuestion()
     {
+        // Get and display question
         questionText.text = question.GetQuestion();
         correctAnswerIndex = question.GetCorrectAnswerIndex();
 
+        // Get and display answers
         for (int i = 0; i < answers.Length; i++)
         {
             TextMeshProUGUI buttonText = answers[i].GetComponentInChildren<TextMeshProUGUI>();
@@ -31,9 +34,35 @@ public class Quiz : MonoBehaviour
         }
     }
 
+    void GetNextQuestion()
+    {
+        // Setup Buttons
+        ChangeButtonStates(true);
+        SetDefaultButtonSprites();
+    }
+
+    void ChangeButtonStates(bool state)
+    {
+        for (int i = 0; i < answers.Length; i++)
+        {
+            Button button = answers[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answers.Length; i++)
+        {
+            Image buttonImage = answers[i].GetComponent<Image>();
+            buttonImage.sprite = defaultSprite;
+        }
+    }
+
     // Check to see if selected answer is correct
     public void OnAnswerSelected(int index)
     {
+        ChangeButtonStates(false);
         // Correct
         if (index == correctAnswerIndex)
         {
